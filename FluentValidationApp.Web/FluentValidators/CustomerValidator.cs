5eb6e2f3189236cdd76fa60ed47204f03d1cc445
dogan.dll <dogandellal@gmail.com>
2022-06-09 11:49:34 +0300

    Add project files.

---
+++ FluentValidationApp.Web/FluentValidators/CustomerValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using FluentValidationApp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentValidationApp.Web.FluentValidators
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+    {
+        public string NotEmptyMessage { get; } = "{PropertyName} alanı boş olamaz";
+        public CustomerValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage(NotEmptyMessage);
+            RuleFor(x => x.Email).NotEmpty().WithMessage(NotEmptyMessage).EmailAddress().WithMessage("Email formatına uygun mail giriniz");
+            RuleFor(x => x.Age).NotEmpty().WithMessage(NotEmptyMessage).InclusiveBetween(18, 60).WithMessage("18 - 60 yaş aralığında olması gerekmektedir");
+            RuleFor(x => x.BirthDay).NotEmpty().WithMessage(NotEmptyMessage).Must(x =>
+            {
+                return DateTime.Now.AddYears(-18) >= x;
+            }).WithMessage("Yaşınız 18 yaşında büyük olmalıdır");
+
+            RuleFor(x => x.Gender).IsInEnum().WithMessage("{PropertyName} alanı erkek için 1 kadın için 2 olmalıdır"); 
+
+            RuleForEach(x => x.Addresses).SetValidator(new AddressValidator());
+        }
+    }
+}
